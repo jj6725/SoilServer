1be862f3d25cb29081086df4ec13124a064bac79
jj6725 <mengdaq@gmail.com>
2019-01-21 02:33:38 +0000

    Updating server to use both soil and temp sensor

--- server.py
+++ server.py
@@ -1,29 +1,67 @@
 #!flask/bin/python
-from flask import Flask, jsonify, make_response
-from sensor import SoilSensor
+from flask import Flask, jsonify, make_response, abort
+from sht31d import SHT31D
+from soilsensor import SoilSensor
 
 ##########################################################################################
 # Python Home Bulb Server
 ##########################################################################################
 
 app = Flask(__name__, static_url_path = "")
-sensor = SoilSensor()
+
+sht31d = Object()
+soils = Object()
+
+try:
+    sht31d = SHT31D()
+except:
+    print("SHT31D not present")
+    pass
+else:
+    sht31d = None
+
+try:
+    soil = SoilSensor()
+except:
+    print("Soil Sensor not present")
+    pass
+else:
+    soil = None
 
 @app.errorhandler(400)
 def bad_request(error):
-	return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+	abort(400)
 
 @app.errorhandler(404)
 def not_found(error):
-	return make_response(jsonify( { 'error': 'Not found' } ), 404)
+    abort(404)
 
 @app.route('/temp', methods = ['GET'])
 def getTemp():
-	return jsonify( {'temperature': sensor.getTemp()} )
+    temp = 0
+
+    if sht31d is not None:
+        temp = sht31d.getTemperature()
+    else if soil is not None:
+        temp = soil.getTemperature()
+    else:
+        abort(404)
+
+    return jsonify( {'temperature': temp})
+
+@app.route('/humidity', methods = ['GET'])
+def getHumidity():
+    if sht31d is not None:
+        return jsonify( {'humidity': sht31d.getHumidity()} )
+    else:
+        abort(404)
 
 @app.route('/moisture', methods = ['GET'])
 def getMoisture():
-	return jsonify( {'moisture': sensor.getMoisture()} )
+    if soil is not None:
+        return jsonify( {'humidity': soil.getMoisture()} )
+    else:
+        abort(404)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=6725, debug=True)
+    app.run(host='0.0.0.0', port=6725, debug=False)
---
+++ sht31d.py
@@ -0,0 +1,30 @@
+import time
+import board
+import busio
+import adafruit_sht31d
+
+class SHT31D:
+    count = 0
+    def getTemp(self):
+        self.count += 1 
+        return self.sensor.temperature
+
+    def getHumidity(self):
+        self.count += 1
+        return self.sensor.relative_humidity
+
+    def heat(self):
+        if self.count > 10:
+            self.sensor.heater = True
+            time.sleep(1)
+            self.sensor.heater = False
+            self.count = 0
+
+    def __init__(self):
+        count = 0 
+        self.sensor = adafruit_sht31d.SHT31D(busio.I2C(board.SCL, board.SDA))
+
+if __name__ == "__main__":
+    sensor = SHT31D()
+    print("\nTemperature: %0.1f C" % sensor.getTemp())
+    print("Humidity: %0.1f %%" % sensor.getHumidity())    
