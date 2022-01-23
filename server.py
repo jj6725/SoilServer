bdd9026037bc446d57e854172d301f9b9097a405
jj6725 <mengdaq@gmail.com>
2022-01-23 20:17:15 +0000

    Disabling debug mode

--- server.py
+++ server.py
@@ -85,4 +85,4 @@ def offlineError():
     return make_response(jsonify( {'error': 'Devices are offline'} ), 500)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=6725, debug=True)
+    app.run(host='0.0.0.0', port=6725, debug=False)
