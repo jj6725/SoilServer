b801b3083923159b9eb067b87a1b4f191b02206e
jj6725 <mengdaq@gmail.com>
2022-11-14 11:51:00 -0600

    Updating server url to support new device manager

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 #!flask/bin/python
 from flask import Flask, jsonify, make_response
 from flask_cors import CORS
+from deviceManager import DeviceManager
 
 app = Flask(__name__, static_url_path="")
 CORS(app)
+
+manager = DeviceManager()
 
 
 @app.errorhandler(400)
@@ -18,12 +21,12 @@ def not_found(error):
 
 @app.route('/devices', methods=['GET'])
 def getDevices():
-    pass
+    return make_response(jsonify(manager.devices))
 
 
 @app.route('/data', methods=['GET'])
 def getData():
-    pass
+    return make_response(jsonify(manager.get_data()))
 
 
 if __name__ == '__main__':
